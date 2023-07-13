bcd7ceb269986b8641b934f84d8e8de091694de0
aslambryantama <bryantama27@gmail.com>
2023-07-13 17:54:09 +0800

    minor hauling

--- pages/3_Coal_Hauling.py
+++ pages/3_Coal_Hauling.py
@@ -26,7 +26,7 @@ day = [5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 night = [17,18,19,20,21,22,23,24,0,1,2,3,4,5,6]
 
 dt_supplier = ["BJS", "WMI", "SUBCON"]
-dt_ktc = ["KTC", "HANVAN", "HAVAN"]
+dt_ktc = ["HANVAN", "HAVAN"]
 
 def cekerror_ch(row):
     ksl = []
