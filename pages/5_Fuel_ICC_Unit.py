53ca57a7fdcf5cbd5ac6bc7f0ccfd25eaa9878a5
aslambryantama <bryantama27@gmail.com>
2023-07-26 17:03:37 +0800

    Initial Fuel Unit Pages

---
+++ pages/5_Fuel_ICC_Unit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from datetime import timedelta
+import dropbox
+import io
+
+st.set_page_config(page_title="KTC | Fuel Unit", page_icon="description/logo.png")
+
+st.title("Fuel ICC Unit")
